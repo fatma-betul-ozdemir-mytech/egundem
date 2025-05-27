6f0a67758d29cdfa36232541b1b748c194d6cfce
fatma-betul-ozdemir-mytech <fbetul23@marun.edu.tr>
2025-05-27 10:27:06 +0300

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('egundem ana sayfa başlığı doğru mu', async ({ page }) => {
   await page.goto('https://egundem.com/');
-  await expect(page).toHaveTitle(/eGündem/);
+  await expect(page).toHaveTitle(/e[-]?gündem/i);
 });
