1c9fe1c7ef2c72f7d4249abb289eb660db55349e
fatma-betul-ozdemir-mytech <fbetul23@marun.edu.tr>
2025-05-27 09:57:50 +0300

    Delete egundem-automation directory

--- egundem-automation/tests/example.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('egundem ana sayfa başlığı doğru mu', async ({ page }) => {
-  await page.goto('https://egundem.com/');
-  await expect(page).toHaveTitle(/eGündem/);
-});
