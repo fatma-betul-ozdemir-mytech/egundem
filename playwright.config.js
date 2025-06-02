d69d5d29a9e3b79ff084f7bca7474d0e500e5cf0
fatma-betul-ozdemir-mytech <fbetul23@marun.edu.tr>
2025-06-02 12:44:28 +0300

    Playwright ve GitHub Actions revize edildi

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,21 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
-  testDir: './tests',
+// playwright.config.js
+
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
   timeout: 60000,
-  failFast: false,  // Hatalı testlerde durma, tüm testleri çalıştır
-  reporter: [
-    ['list'],
-    ['json', { outputFile: 'playwright-report/results.json' }],
-    ['html', { outputFolder: 'playwright-report/html-report', open: 'never' }],
-  ],
+  retries: 0,
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
+    video: 'off',
     screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+    baseURL: 'https://egundem.com',
   },
-};
-
-module.exports = config;
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
+});
