75711ccaadf9957a117038e6c53bf79e6aedd0a9
fatma-betul-ozdemir-mytech <fbetul23@marun.edu.tr>
2025-06-02 11:48:58 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,10 @@
-// playwright.config.js
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   testDir: './tests',
   timeout: 60000,
   reporter: [
     ['list'],
-    ['json', { outputFile: 'playwright-report/results.json' }]
+    ['json', { outputFile: 'playwright-report/results.json' }],
+    ['html', { outputFolder: 'playwright-report/html-report', open: 'never' }]
   ],
   use: {
     headless: true,
